b6702d1e26e8af334adb5731aed9380c3b259079
Igor Silveira <up201505172@fe.up.pt>
2019-04-25 12:31:12 +0100

    Service Provider added for perms

--- app/Providers/PermissionsServiceProvider.php
+++ app/Providers/PermissionsServiceProvider.php
@@ -25,5 +25,25 @@ class PermissionsServiceProvider extends ServiceProvider
      * @return void
      */
     public function boot()
-    { }
+    {
+        Permission::get()->map(function ($permission) {
+            Gate::define($permission->slug, function ($user) use ($permission) {
+                return $user->hasPermissionTo($permission);
+            });
+        });
+
+        Blade::directive('role', function ($role) {
+            return "<?php if(auth()->check() && auth()->user()->hasRole({$role})) {?>";
+        });
+        Blade::directive('endrole', function ($role) {
+            return "<?php } ?>";
+        });
+
+        Blade::directive('permission', function ($permission) {
+            return "<?php if(auth()->check() && auth()->user()->can({$permission})) {?>";
+        });
+        Blade::directive('endpermission', function ($permission) {
+            return "<?php } ?>";
+        });
+    }
 }
